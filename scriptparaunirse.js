e2e3432a2966f76fac8be156f38bfdd9f014536a
Cristina Gutierrez <88104698+gutierrezc@users.noreply.github.com>
2023-10-31 19:38:31 -0500

    Add files via upload

---
+++ scriptparaunirse.js
@@ -0,0 +1,17 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
+
+('click', function() {
+    var mensaje = "¡Registro exitoso! Bienvenido a The elixir of life.";
+
+    alert(mensaje);
+});
